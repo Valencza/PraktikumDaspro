652207a741bd92282b78616ae6712ae03bb2280c
Valencza <143587759+Valencza@users.noreply.github.com>
2025-10-02 10:15:06 +0700

    Tugas 2

---
+++ Jobsheet5/ifElseGanjilGenap11.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+
+public class ifElseGanjilGenap11 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("--- Program Cek Ganjil/Genap ---");
+        System.out.print("Masukkan sebuah angka: ");
+        int angka = sc.nextInt();
+
+        if (angka % 2 == 0) {
+            System.out.println("Genap");
+        } else {
+            System.out.println("Ganjil");
+        }
+    }
+}
