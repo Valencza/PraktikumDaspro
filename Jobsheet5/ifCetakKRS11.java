5b585378b74051d64d69f9ba3055436436cb70d0
Valencza <143587759+Valencza@users.noreply.github.com>
2025-10-02 10:09:59 +0700

    Tugas 1

--- Jobsheet5/ifCetakKRS11.java
+++ Jobsheet5/ifCetakKRS11.java
@@ -5,14 +5,23 @@ public class ifCetakKRS11 {
         Scanner sc = new Scanner(System.in);
 
         System.out.println("--- Cetak KRS SIAKAD ---");
-        System.out.print("Apakah UKT sudah lunas? (true/false):");
+        System.out.print("Apakah UKT sudah lunas? (true/false): ");
         boolean uktLunas = sc.nextBoolean();
 
+        // Bentuk IF-ELSE biasa
         if (uktLunas) {
             System.out.println("Pembayaran UKT terverifikasi");
-            System.out.print("Silahkan cetak KRS dan minta tanda tangan DPA");
-        }else{
+            System.out.println("Silahkan cetak KRS dan minta tanda tangan DPA");
+        } else {
             System.out.println("Registrasi ditolak. Silahkan lunasi UKT terlebih dahulu.");
         }
+
+        // Transformasi ke Ternary Operator
+        String pesan = uktLunas 
+            ? "Pembayaran UKT terverifikasi\nSilahkan cetak KRS dan minta tanda tangan DPA" 
+            : "Registrasi ditolak. Silahkan lunasi UKT terlebih dahulu.";
+
+        System.out.println("\n--- Versi Ternary Operator ---");
+        System.out.println(pesan);
     }
 }
